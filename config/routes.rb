82b533de61391490ef6ec6da41fef57df2a4c018
osha7 <ogroetz@gmail.com>
2023-10-23 02:09:20 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: "static#home"
 
   devise_for :users
-
+  resources :users
+  
   resources :images
+  put "/image_upload" => "images#image_upload"
+
   resources :tailoring_sizes
   resources :listing_shipping_statuses
   resources :cart_listings
@@ -19,5 +22,16 @@ Rails.application.routes.draw do
   resources :orders
   resources :types
   resources :stores
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/create_express_account" => "users#create_express_account"
+  post "/create_account_link" => "users#create_account_link"
+  get "/retrieve_express_account" => "users#retrieve_express_account"
+
+  namespace :api do
+    namespace :v1 do
+    end
+  end
+
 end
