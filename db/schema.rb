f63eae6ca8f5be7774d2bdf435bf53b3f39f8aa9
osha7 <ogroetz@gmail.com>
2023-10-23 01:10:21 +0000

    cors and auth token on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_22_232956) do
+ActiveRecord::Schema.define(version: 2023_10_23_010904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -227,6 +227,8 @@ ActiveRecord::Schema.define(version: 2023_10_22_232956) do
     t.integer "reviews_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
